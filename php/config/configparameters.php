50a0b241e5422854ec9477376c1477c331aa79f2
Pilar Avila <pavila@medallia.com>
2022-06-30 16:24:43 -0300

    parametros de host heroku

--- php/config/configparameters.php
+++ php/config/configparameters.php
@@ -1,12 +1,12 @@
 <?php
 
-$path_host = "http://localhost:3000";
+$path_host = "https://comiteslucha.herokuapp.com";
 $path_noticias = $path_host."/php/noticias/";
 $path_obtener_noticias = $path_host."/noticias/obtener.php";
 $path_luchadores = $path_host."/php/luchadores/";
 $path_obtener_luchadores = $path_host."/luchadores/obtener.php";
 $path_actualidad = $path_host."/php/actualidad/";
 $path_obtener_actualidad = $path_host."/actualidad/obtener.php";
-$path_location =  "http://localhost:3000";
+$path_location =  "https://comiteslucha.herokuapp.com";
  
 ?>
